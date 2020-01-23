572e43a963541fca6812f5eb2169ea2e24fda1e2
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-23 14:18:05 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,9 @@
 # Build your say_hello method here
-def say_hello()
-  puts "Hello Ruby Programmer!"
-end
+
 def say_hello (name)
-  puts "Hello #{name}!"
+  if name
+    puts "Hello #{name}!"
+  else
+    puts "Hello Ruby Programmer"
+  end
 end
